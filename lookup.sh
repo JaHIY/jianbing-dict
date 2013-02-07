35fa77645228b6ef1cddea684c43e3b58ffac91d
JaHIY <jaklsy@gmail.com>
2013-02-07 12:21:10 +0800

    optimize code

--- lookup.sh
+++ lookup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -
 
+BASE_URL='jianbing.org'
+
 char() {
-    local ascii_oct_code
     while IFS='' read -r line
     do
         case "$line" in
-            \\[[:digit:]][[:digit:]][[:digit:]])
-                ascii_oct_code=$(cut -c '2-4' <<< "$line")
-                printf $(bc <<< "obase=16;${ascii_oct_code}" | sed 's/^.*$/\\x&/')
+            \\[[:digit:]][[:digit:]][[:digit:]]*)
+                printf '%b' $(awk 'BEGIN{FS="\\"}{for(i=2;i<=NF;i++){printf "\\x%x", $i}}' <<< "$line")
                 ;;
             *)
                 printf '%s' "$line"
@@ -20,8 +20,8 @@ main() {
     while [ $# -gt 0 ]
     do
         printf '%s\n' "$1"
-        dig "$1.jianbing.org" txt +short +tcp | sed -e 's/^"\(.*\)"$/\1\\010/g' | \
-            grep -o '\\[[:digit:]]\{3\}\|.' | char
+        dig "${1}.${BASE_URL}" txt +short +tcp | sed -e 's/^"\(.*\)"$/\1\\010/g' | \
+            grep -o '\\[[:digit:]]\{3\}\+\|.' | char
         printf '\n'
         shift
     done
